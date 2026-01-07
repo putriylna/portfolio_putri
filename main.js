064c5a0af412be86cbe4e77a4fd3b379d9b24124
Putri Yuliana <yulianarahma2008.com>
2026-01-07 08:15:21 +0700

    Changed

--- main.js
+++ main.js
@@ -129,3 +129,27 @@
       }, 300);
     });
   });
+
+  /* ===============================
+      SWIPER JS INITIALIZATION
+  =============================== */
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+
+  breakpoints: {
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 },
+  },
+});
